7091add468b07cd436ae271ed22d91d1038a9237
Escalante Antonio <antonio.escalante@mxc.mcd.com>
2025-04-05 21:23:08 -0600

    feat: Added challenge 'the anagram finder' in python

---
+++ logic/python/the-anagram-finder.py
@@ -0,0 +1,32 @@
+##############################################
+#### Coding Challenge: The Anagram Finder ####
+##############################################
+
+### Dificulty: Medium
+### Problem Statement
+# You're given a list of words and a target word.
+# Your task is to write a function that returns all
+# the anagrams of the target workd from the list.
+#
+# An anagram is a word phrase formed by
+# rearranging the letters of a different word or
+# phrase, typically using all  the original lettters
+# exactly once.
+
+# Inputs
+target = "listen"
+words = [
+    "enlist",
+    "google",
+    "inlets",
+    "banana",
+    "silent",
+    "tinsel",
+    "listen"
+    ]
+
+def find_anagrams(target: str, words: list[str]) -> list[str]:
+    
+
+
+print(find_anagrams(target, words))
